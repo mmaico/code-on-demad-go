ee05a45cab83b2424f65e1edbf4c808249bcd49f
Marcelo Maico <peo_mjesus@uolinc.com>
2023-04-08 18:14:47 -0300

    Config v8 to get javascript from http

--- contacts/view/support/V8Engine.go
+++ contacts/view/support/V8Engine.go
@@ -1,23 +1,30 @@
 package support
 
 import (
-	"os"
+	"io"
+	"net/http"
 	v8 "rogchap.com/v8go"
 )
 
 func MaskData(value string) string {
-	content, _ := os.ReadFile("/Users/peo_mjesus/mmaico/code-on-demand/src/main/resources/js-code/json-mask.js")
+	baseUrl := "http://localhost:3000/json-mask.js"
+
+	client := http.Client{}
+
+	resp, _ := client.Get(baseUrl)
+	defer resp.Body.Close()
+	bodyStr, _ := io.ReadAll(resp.Body)
 
 	iso := v8.NewIsolate()
 	global := v8.NewObjectTemplate(iso)
 	ctx := v8.NewContext(global, iso)
 
-	ctx.RunScript(string(content), "json-our.js")
+	ctx.RunScript(string(bodyStr), "json-our.js")
 
 	json := "{\"value\": \"" + value + "\" }"
 	setting := "[{\"type\": \"" + "MOBILE" + "\", \"path\": \"$.value\"}]"
 
-	val, _ := ctx.RunScript("EntryPoint.default("+json+","+setting+")", "/Users/peo_mjesus/mmaico/code-on-demand/src/main/resources/js-code/json-mask.js")
+	val, _ := ctx.RunScript("EntryPoint.default("+json+","+setting+")", "json-mask.js")
 
 	return val.String()
 }
